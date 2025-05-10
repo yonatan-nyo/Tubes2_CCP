44ffe36d9bfef52cce8e52752da2575526ce3292
varelll <19623183@std.stei.itb.ac.id>
2025-05-10 20:27:22 +0700

    add about us page

--- src/frontend/src/pages/Layout.tsx
+++ src/frontend/src/pages/Layout.tsx
@@ -42,6 +42,16 @@ function Navbar() {
             >
               Visualizer
             </Link>
+            <Link 
+              to="/AboutUs" 
+              className={`px-3 py-2 rounded-md text-sm font-medium ${
+                location.pathname === '/AboutUs' 
+                  ? 'bg-blue-900 text-white' 
+                  : 'text-blue-100 hover:bg-blue-600'
+              }`}
+            >
+              About Us
+            </Link>
           </div>
         </div>
       </div>
@@ -69,6 +79,7 @@ function Footer() {
               <p><Link to="/" className="text-sm hover:text-white">Home</Link></p>
                 <p><Link to="/Wiki" className="text-sm hover:text-white">Wiki</Link></p>
                 <p><Link to="/Visualizer" className="text-sm hover:text-white">Visualizer</Link></p>
+                <p><Link to="/AboutUs" className="text-sm hover:text-white">About Us</Link></p>
               </div>
                 
             </div>
